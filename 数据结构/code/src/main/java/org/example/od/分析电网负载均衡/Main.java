1301bd6c76aeaf1567ef6f07740d2e7a7a361a1b
tainger <598424640@qq.com>
2026-08-24 09:06:00 +0800

    commit

--- 数据结构/code/src/main/java/org/example/od/分析电网负载均衡/Main.java
+++ 数据结构/code/src/main/java/org/example/od/分析电网负载均衡/Main.java
@@ -1,4 +1,3 @@
-
 package org.example.od.分析电网负载均衡;
 
 /**
@@ -6,6 +5,22 @@ package org.example.od.分析电网负载均衡;
  * @date 2026/8/24 07:41
  */
 public class Main {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    // loads: 节点负载数组（长度 N）
+// edges: 输电线路二维数组，每行 [u, v] 表示两节点相连
+// 返回最大不均衡度；无有效供电区返回 -1
+    public int maxImbalance(int[] loads, int[][] edges){
+
+
+
+
+
+    }
 }
 
 
--- 数据结构/code/src/main/java/org/example/od/寻找孤立水站/Main.java
+++ 数据结构/code/src/main/java/org/example/od/寻找孤立水站/Main.java
@@ -1,11 +1,85 @@
-
 package org.example.od.寻找孤立水站;
+
+
+import java.util.*;
 
 /**
  * @author jiazhiyuan
  * @date 2026/8/24 07:54
  */
 public class Main {
+
+    public static void main(String[] args) {
+
+        int n = 5;
+        int[] sources = {1};
+        int[][] pipes = {{1, 0, 0},{1, 2, 0}};
+
+
+
+    }
+
+
+    /**
+     *
+     * @param n
+     * @param sources
+     * @param pipes
+     * @return
+     */
+    public List<Integer> findIsolated(int n, int[] sources, int[][] pipes) {
+
+        List<List<Integer>> graph = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for(int i =0; i< pipes[0].length; i++) {
+            int v = pipes[i][0];
+            int e = pipes[i][1];
+            int type = pipes[i][2];
+            graph.get(v).add(e);
+
+            if(type == 1) {
+                graph.get(e).add(v);
+            }
+        }
+
+        boolean[] visited = new boolean[n];
+
+        Queue<Integer> queue = new ArrayDeque<>();
+
+        //先把源头设置访问了
+
+        for(int i = 0; i < sources.length; i++) {
+            if(!visited[sources[i]]) {
+                visited[sources[i]] =true;
+                queue.offer(sources[i]);
+            }
+        }
+
+        //开始bfs
+
+        while(!queue.isEmpty()) {
+            int u = queue.poll();
+            for(int v : graph.get(u)) {
+                if(!visited[v]) {
+                    visited[v] =true;
+                    queue.offer(v);
+                }
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+
+        for (int i = 0; i < visited.length; i++) {
+            if(! visited[i]) {
+                ans.add(i);
+            }
+        }
+        return  ans;
+    }
 }
 
 
