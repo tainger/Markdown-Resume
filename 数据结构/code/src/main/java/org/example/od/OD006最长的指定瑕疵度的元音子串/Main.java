09c31477162d1e38082b7c3115822bbaccf713be
tainger <598424640@qq.com>
2026-08-25 14:38:43 +0800

    docs: 更新算法文档，补充华为OD6题示例和树状DP解法
    1.  在滑动窗口文档中新增最长瑕疵度元音子串示例
    2.  补全华为OD6题的Java实现代码
    3.  重构树状DP题解，拆分三种解法并添加对比表格
    4.  补充常见坑点和复杂度分析细节

--- 数据结构/code/src/main/java/org/example/od/OD006最长的指定瑕疵度的元音子串/Main.java
+++ 数据结构/code/src/main/java/org/example/od/OD006最长的指定瑕疵度的元音子串/Main.java
@@ -1,4 +1,3 @@
-
 package org.example.od.OD006最长的指定瑕疵度的元音子串;
 
 /**
@@ -6,6 +5,45 @@ package org.example.od.OD006最长的指定瑕疵度的元音子串;
  * @date 2026/8/25 13:48
  */
 public class Main {
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public int longestVowelSubstring(String s, int k) {
+        int n = s.length();
+        int left = 0;
+        int cnt = 0;
+        int ans = 0;
+
+        for (int right = 0; right < n; right++) {
+            if(!isVoel(s.charAt(right))) {
+                cnt ++;
+            }
+
+            while (cnt > k) {
+                if(!isVoel(s.charAt(left))){
+                    cnt --;
+                }
+                left++;
+            }
+
+            if(cnt  == k && isVoel(s.charAt(left)) && isVoel(s.charAt(right))) {
+
+                ans = Math.max(ans, right - left + 1);
+            }
+        }
+        return  ans;
+    
+    }
+
+    private boolean isVoel(char c) {
+        return "aeiouAEIOU".indexOf(c) >= 0;
+    }
+
+
 }
 
 
