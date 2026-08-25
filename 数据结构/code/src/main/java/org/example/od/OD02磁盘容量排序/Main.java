9a92e348c0163b09fe7c0ce0d2e72d92fe0dd12e
tainger <598424640@qq.com>
2026-08-25 11:53:34 +0800

    feat(od): 补充华为OD机试题笔记与Java题解
    - 3.发广播：新增「统计根节点数 parent[i] vs find(i)」踩坑分析（并查集路径压缩惰性）
    - 4.单词接龙：改写为 LeetCode 风格（函数签名/示例/Constraints/复杂度分析）
    - OD02磁盘容量排序/Main.java：补充题解
    - OD03发广播/Main.java：补充题解

--- 数据结构/code/src/main/java/org/example/od/OD02磁盘容量排序/Main.java
+++ 数据结构/code/src/main/java/org/example/od/OD02磁盘容量排序/Main.java
@@ -1,4 +1,3 @@
-
 package org.example.od.OD02磁盘容量排序;
 
 /**
@@ -6,6 +5,32 @@ package org.example.od.OD02磁盘容量排序;
  * @date 2026/8/25 09:16
  */
 public class Main {
+
+    public static void main(String[] args) {
+
+    String[] disks = new String[]{"1T","20M","3G"};
+
+    }
+
+
+    public String[] sortDiskCapacity(String[] disks) {
+
+        int[] mbArray = transform(disks);
+
+
+
+
+        return new String[]{"ok"};
+    }
+
+    private int[] transform(String[] disks) {
+        for (int i = 0; i < disks.length; i++) {
+            String disk = disks[i];
+
+        }
+
+        return new int[]{};
+    }
 }
 
 
--- 数据结构/code/src/main/java/org/example/od/OD03发广播/Main.java
+++ 数据结构/code/src/main/java/org/example/od/OD03发广播/Main.java
@@ -1,11 +1,57 @@
-
 package org.example.od.OD03发广播;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author jiazhiyuan
  * @date 2026/8/25 11:17
  */
 public class Main {
+
+    static int[] parent;
+
+    static  int find(int x) {
+        if(parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    static  void union(int a, int b) {
+        int pa = find(a);
+        int pb = find(b);
+        if(pa != pb) {
+            parent[pb] = pa;
+        }
+    }
+
+    public int broadcast(int n, String[] grid) {
+
+        parent = new int[n];
+
+
+        for(int i = 0; i < n; i++) {
+            for(int  j =0 ; j < n ;j ++) {
+
+                if(grid[i].charAt(j) == '1') {
+                    union(i,j);
+                }
+            }
+        }
+
+        Set<Integer> roots = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            roots.add(find(i));
+        }
+
+        return roots.size();
+    }
+    public static void main(String[] args) {
+
+
+    }
 }
 
 
