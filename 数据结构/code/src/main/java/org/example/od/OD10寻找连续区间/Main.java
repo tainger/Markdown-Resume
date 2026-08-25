8df5cddb42dcee4eae806f1dd5187b21c4eed1ca
tainger <598424640@qq.com>
2026-08-25 15:49:21 +0800

    feat(OD10): add sliding window and brute force solution skeleton
    add main test method for OD10 problem, implement initial sliding window count logic, and create empty brute force method

--- 数据结构/code/src/main/java/org/example/od/OD10寻找连续区间/Main.java
+++ 数据结构/code/src/main/java/org/example/od/OD10寻找连续区间/Main.java
@@ -1,4 +1,3 @@
-
 package org.example.od.OD10寻找连续区间;
 
 /**
@@ -6,6 +5,54 @@ package org.example.od.OD10寻找连续区间;
  * @date 2026/8/25 15:17
  */
 public class Main {
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 4, 5};
+        int x = 7;
+        long l = new Main().countIntervals(nums, x);
+        System.out.println(l);
+
+    }
+
+    //滑动窗口
+    public long countIntervals(int[] nums, int x) {
+        // ...
+        int right = 0;
+
+        int left = 0;
+
+        int n = nums.length;
+
+        int sum = 0;
+
+        int countRange = 0;
+        while (right < n &&  left <=right) {
+
+            sum += nums[right];
+
+            if (sum >= x) {
+                countRange ++;
+            }
+
+            right++;
+
+            if(right > n - 1) {
+                left++;
+                right = left;
+                sum =0;
+            }
+        }
+
+        return countRange;
+    }
+
+    public long countIntervalsForce(int[] nums, int x) {
+        // ...
+
+
+        return 1;
+    }
 }
 
 
