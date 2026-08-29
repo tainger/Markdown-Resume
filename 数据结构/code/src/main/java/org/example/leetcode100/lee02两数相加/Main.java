e255e0b3cb9130146ce57fe9e4dbda6a69e46495
tainger <598424640@qq.com>
2026-08-29 10:29:29 +0800

    docs: add two attempts and code for leetcode 2 add two numbers
    补充了两数相加题目的两次解题尝试代码，包括复盘用的初次冗余实现和存在精度问题的求和实现

--- 数据结构/code/src/main/java/org/example/leetcode100/lee02两数相加/Main.java
+++ 数据结构/code/src/main/java/org/example/leetcode100/lee02两数相加/Main.java
@@ -24,15 +24,53 @@ public class Main {
     }
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
 
-        
+        if (l2 == null) {
+            return l1;
+        }
 
-        return new ListNode();
+        int sumL1 = 0;
+        int countl1 = 1;
+        while (l1 != null) {
+            sumL1 += l1.val * countl1 + sumL1;
+            l1 = l1.next;
+            countl1 = countl1 * 10;
+        }
+
+        int sumL2 = 0;
+        int countl2 = 1;
+
+        while (l2 != null) {
+            sumL2 = l2.val * countl2 + sumL2;
+            l2 = l2.next;
+            countl2 = countl2 * 10;
+        }
+
+        int sum = sumL1 + sumL2;
+        ListNode yummy = new ListNode(-1);
+        ListNode cur = yummy;
+
+        while (sum > 0) {
+            int yushu = sum % 10;
+            ListNode yushuListNode = new ListNode(yushu);
+            cur.next = yushuListNode;
+            cur = cur.next;
+            sum = sum / 10;
+
+        }
+        return yummy.next;
+
     }
 
 
     public static void main(String[] args) {
+        Main main = new Main();
 
+
+        main.addTwoNumbers();
     }
 }
 
