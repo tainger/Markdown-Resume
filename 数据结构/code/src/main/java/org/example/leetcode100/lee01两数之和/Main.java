4136bdce4ffae3de43d02b47eca92a660fdb1ef0
tainger <598424640@qq.com>
2026-08-29 09:58:09 +0800

    chore: 整理算法思想笔记并完善目录索引
    1. 新增/完善多篇算法思想文档：枚举、栈、队列、链表、哈希表、状态机、数组、矩阵、字符串、进制转换、正则表达式、排列组合、BFS/DFS搜索等
    2. 修正原错别字文件名，删除重复空文件
    3. 重构算法思想目录的README索引，补充完整分类与易混思想分界说明
    4. 补充leetcode对应题目的关联笔记链接

--- 数据结构/code/src/main/java/org/example/leetcode100/lee01两数之和/Main.java
+++ 数据结构/code/src/main/java/org/example/leetcode100/lee01两数之和/Main.java
@@ -1,5 +1,7 @@
 package org.example.leetcode100.lee01两数之和;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -10,7 +12,57 @@ import java.util.concurrent.ConcurrentHashMap;
 public class Main {
 
     public static void main(String[] args) {
-        Map<String, String> map = new ConcurrentHashMap();
+
+    }
+
+    /**
+     * 1.双重for循环暴力
+     * 2.hashmap
+     * 3.sort 之后双指针。双指针不行，排序之后会把下标换掉。
+     */
+    public int[] twoSum1(int[] nums, int target) {
+
+        for(int i = 0; i < nums.length; i ++) {
+            for(int j = i + 1; j < nums.length; j++) {
+                if(target == nums[i] + nums[j]) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{-1, -1};
+
+    }
+    public int[] twoSum2(int[] nums, int target) {
+
+        Map<Integer, Integer> map  = new HashMap<>();
+        for (int i = 0; i <nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i =0 ; i < nums.length;i++) {
+            int nextValue = target - nums[i];
+
+            Integer integer = map.get(nextValue);
+            if(integer != null && integer != i)  {
+                return new int[]{i, integer};
+            }
+        }
+        return new int[]{-1, -1};
+    }
+    public int[] twoSum3(int[] nums, int target) {
+        Arrays.sort(nums);
+        int j = 0;
+        int k = nums.length -1 ;
+        while (j < k) {
+            if(target > nums[j] + nums[k]) {
+               j++;
+            }else if(target < nums[j] + nums[k]) {
+                k--;
+            }else {
+                return new int[]{j, k};
+            }
+        }
+        return new int[]{-1, -1};
     }
 }
 
--- 数据结构/code/src/main/java/org/example/leetcode100/lee02两数相加/Main.java
+++ 数据结构/code/src/main/java/org/example/leetcode100/lee02两数相加/Main.java
@@ -6,6 +6,31 @@ package org.example.leetcode100.lee02两数相加;
  */
 public class Main {
 
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        
+
+        return new ListNode();
+    }
+
+
     public static void main(String[] args) {
 
     }
