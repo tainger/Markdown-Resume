c6449cfa6bc303144d3d21a51b482b57621b98f9
tainger <598424640@qq.com>
2026-08-31 12:34:43 +0800

    feat(leetcode100/leet05): add center expansion solution and docs update
    add center expansion implementation for longest palindromic substring problem, also update the problem documentation with solution notes and new sliding window practice solution

--- 数据结构/code/src/main/java/org/example/leetcode100/leet05最长回文子串/Main.java
+++ 数据结构/code/src/main/java/org/example/leetcode100/leet05最长回文子串/Main.java
@@ -1,4 +1,3 @@
-
 package org.example.leetcode100.leet05最长回文子串;
 
 /**
@@ -6,6 +5,46 @@ package org.example.leetcode100.leet05最长回文子串;
  * @date 2026/8/31 11:58
  */
 public class Main {
+
+    int  maxlen = 1;
+
+    int start = 0;
+    public static void main(String[] args) {
+
+        String s = "babad";
+        String s2 = "cbbd";
+
+        String s1 = new Main().longestPalindrome(s);
+        String s3 = new Main().longestPalindrome(s2);
+
+        System.out.println(s1);
+        System.out.println(s3);
+
+
+    }
+
+    public String longestPalindrome(String s) {
+        char[] charArray = s.toCharArray();
+        int n = charArray.length - 1;
+
+        for (int i = 0; i < charArray.length; i++) {
+             int j =  i -1;
+             int k =  i + 1;
+             int len = 1;
+             while (j >= 0 && k <= n) {
+                 if (charArray[j] == charArray[k]) {
+                     maxlen = Math.max(maxlen, len + 2);
+                     start = j;
+                     j--;
+                     k++;
+                 }else {
+                     break;
+                 }
+             }
+
+        }
+        return s.substring(start, maxlen+1);
+    }
 }
 
 
