c651b5d502bae1d348519ef6ea66dc91a82dc4cf
tainger <598424640@qq.com>
2026-08-31 19:01:51 +0800

    docs: add 53.最大子数组和题解及相关代码
    新增LeetCode Hot100第53题最大子数组和的详细题解文档，同时补充了该题的动态规划实现代码，以及零钱兑换的回溯暴力解法框架

--- 数据结构/code/src/main/java/org/example/dp/chapter02/MainCoin.java
+++ 数据结构/code/src/main/java/org/example/dp/chapter02/MainCoin.java
@@ -1,4 +1,3 @@
-
 package org.example.dp.chapter02;
 
 /**
@@ -6,6 +5,45 @@ package org.example.dp.chapter02;
  * @date 2026/8/31 17:05
  */
 public class MainCoin {
+
+    private int ans = Integer.MAX_VALUE;
+
+    public int coinChange1(int[] coins, int amount) {
+        backtrack(coins, amount, 0);
+        return ans == Integer.MAX_VALUE ?  -1: ans;
+
+    }
+
+    /**
+     *
+     * @param coins
+     * @param rest
+     * @param count
+     */
+    private void backtrack(int[] coins, int rest, int count) {
+        if(rest == 0) {
+            ans = Math.min(ans, count);
+            return;
+        }
+        //剪枝条件
+        if(count > ans) {
+            return;
+        }
+
+        for(int coin: coins) {
+            if(coin < rest) {
+                backtrack(coins, rest - coin, count + 1);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+
+
 }
 
 
--- 数据结构/code/src/main/java/org/example/dp/chapter04/Main.java
+++ 数据结构/code/src/main/java/org/example/dp/chapter04/Main.java
@@ -1,4 +1,3 @@
-
 package org.example.dp.chapter04;
 
 /**
@@ -6,6 +5,39 @@ package org.example.dp.chapter04;
  * @date 2026/8/31 17:48
  */
 public class Main {
+
+
+    public static void main(String[] args) {
+        int[] array = new int[]{-2, 1, -3, 1, -1, 6, 2, -5, 4};
+    }
+
+
+    /**
+     * dp[i]= max{dp[i-1] + nums[i], nums[i]}
+     *
+     * @param nums
+     * @return
+     */
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return -1;
+        }
+        if (n == 1) {
+            return nums[0];
+        }
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        for(int i=1; i <= n -1 ;i++) {
+
+            if(nums[i]> dp[i-1] + nums[i] ) {
+                dp[i] = nums[i];
+            }else {
+                dp[i] = dp[i-1] + nums[i];
+            }
+        }
+        return dp[n-1];
+    }
 }
 
 
