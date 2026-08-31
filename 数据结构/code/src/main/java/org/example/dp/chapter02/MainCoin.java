bd53dac5b204ba857bfc237e2ae9ead22db296da
tainger <598424640@qq.com>
2026-08-31 19:01:44 +0800

    feat(dp): add empty main classes for chapter 2 and 4
    新增了动态规划第2章和第4章的空主类文件，用于后续章节代码开发

---
+++ 数据结构/code/src/main/java/org/example/dp/chapter02/MainCoin.java
@@ -0,0 +1,13 @@
+
+package org.example.dp.chapter02;
+
+/**
+ * @author jiazhiyuan
+ * @date 2026/8/31 17:05
+ */
+public class MainCoin {
+}
+
+
+
+    
---
+++ 数据结构/code/src/main/java/org/example/dp/chapter04/Main.java
@@ -0,0 +1,13 @@
+
+package org.example.dp.chapter04;
+
+/**
+ * @author jiazhiyuan
+ * @date 2026/8/31 17:48
+ */
+public class Main {
+}
+
+
+
+    
