a6f87f5b740afd04d3d17f14056576e57ff8af97
tainger <598424640@qq.com>
2026-08-31 19:42:51 +0800

    feat(leetcode-hot100): add 152. 乘积最大子数组题解
    新增LeetCode热题100第152题乘积最大子数组的完整题解，包括题目说明、暴力/DP两种解法、细节坑点总结和同源题族对比，同时更新总表索引。

--- 数据结构/code/src/main/java/org/example/leetcode100/lee152乘积最大子数组/Main.java
+++ 数据结构/code/src/main/java/org/example/leetcode100/lee152乘积最大子数组/Main.java
@@ -1,4 +1,3 @@
-
 package org.example.leetcode100.lee152乘积最大子数组;
 
 /**
@@ -6,6 +5,25 @@ package org.example.leetcode100.lee152乘积最大子数组;
  * @date 2026/8/31 19:30
  */
 public class Main {
+
+    public static void main(String[] args) {
+        int[] nums = {-2,3,-4};
+        int res = new Main().maxProduct(nums);
+        System.out.println(res);
+    }
+
+    public int maxProduct(int[] nums) {
+        int n = nums.length;
+        int endRes = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            int res = nums[i];
+            for (int j = i + 1; j < n; j++) {
+                res = res * nums[j];
+                endRes = Math.max(endRes, res);
+            }
+        }
+        return endRes;
+    }
 }
 
 
