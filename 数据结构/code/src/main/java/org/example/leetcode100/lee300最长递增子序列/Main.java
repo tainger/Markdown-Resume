9899c05406f80e22ee1c521f8db0fd172c1018ac
tainger <598424640@qq.com>
2026-09-01 11:17:07 +0800

    feat(leetcode-hot100): add 300. 最长递增子序列题解和代码
    新增leetcode热题100第300题的题目文档、Java实现代码，并更新了README表格，包含DP和贪心+二分两种解法的完整讲解

--- 数据结构/code/src/main/java/org/example/leetcode100/lee300最长递增子序列/Main.java
+++ 数据结构/code/src/main/java/org/example/leetcode100/lee300最长递增子序列/Main.java
@@ -1,11 +1,74 @@
-
 package org.example.leetcode100.lee300最长递增子序列;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author jiazhiyuan
  * @date 2026/9/1 10:24
  */
 public class Main {
+
+    private List<List<Integer>> res = new ArrayList<>();
+    public static void main(String[] args) {
+        int[] nums = new int[]{10,9,2,5,3,7,101,18};
+        int i = new Main().lengthOfLIS(nums);
+        System.out.println(i);
+    }
+
+    /**
+     *
+     *
+     * dp[i][j]= {
+     *     i = j ,    dp[i][j] = 1
+     *     i + 1= j   nums[j], dp[i][j] = max{dp[i][j-1],  dp[i][j-1]}
+     *
+     * }
+     * @param nums
+     * @return
+     */
+    public int lengthOfLIS(int[] nums) {
+
+
+    }
+
+
+    private void dfs (int[] nums, int index) {
+
+        if(index > nums.length) {
+            return ;
+        }
+
+        for (int i = index; i < nums.length; i++) {
+
+
+        }
+
+    }
+
+
+
+        /**
+         * 第一次尝试么有暴力解，子序列，并不是
+         * @param nums
+         * @return
+         */
+    public int lengthOfLIS(int[] nums) {
+        int resLen = 1;
+        for(int i = 0; i< nums.length; i ++)  {
+            int len = 1;
+            for(int j = i + 1; j < nums.length; j++) {
+                if(nums[j] > nums[j-1]) {
+                    len++;
+                    resLen = Math.max(resLen, len);
+                }else {
+                    break;
+                }
+            }
+        }
+        return resLen;
+    }
+
 }
 
 
